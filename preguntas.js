ce52fb3e75bcda5355175493fe7a0241f79b0b3f
enragedjam <enragedjam@gmail.com>
2017-05-29 13:25:44 +0200

    asdfsss

--- preguntas.js
+++ preguntas.js
@@ -1,11 +1,3 @@
-$.ajax({
-   url: 'http://diegocollado.net/preguntas.html',
-   type: 'GET',
-   success: function(res) {
-      var data = $.parseHTML(res); 
-      $(data).find('.preguntas').each(function(){
-          $('#preguntas2').append($(this).html());
-     });
-
-   }
- });
+jQuery(function($){
+	$('#preguntas').load('preguntas.html');
+});
